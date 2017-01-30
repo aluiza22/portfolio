1100dc7c11ee69a308ece2e9cd8af801d8066f63
Ana <aluiza.silva22@gmail.com>
2017-01-30 18:00:47 -0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function svgToBase64Image(svgElement) {
+  var div = document.createElement('div');
+  div.appendChild(svgElement.cloneNode(true));
+  var b64 = window.btoa(div.innerHTML);
+  return 'data:image/svg+xml;base64,' + b64;
+}
+var svgs = document.getElementsByTagName('svg');
+var urls = [];
+for (var i = 0; i < svgs.length; i++)
+  urls.push('url("' + svgToBase64Image(svgs[i]) + '")');
+var url = urls.join(',');
+var header = document.getElementsByTagName('header')[0];
+header.style.background = url;
+var titles = document.querySelectorAll('h2.sectionTitle');
+titles.forEach(title => title.style.background = url);
